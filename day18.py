adc8681990a8017a8d0d0d3fd08a2a953fddc69f
Joris van Vugt <jorisvanvugt@gmail.com>
2019-12-26 11:49:53 +0100

    Hacky solution for 18

--- day18.py
+++ day18.py
@@ -66,14 +66,13 @@ def a(world):
                 queue.append(State(neighbour, keys, state.num_steps + 1))
 
 
-Pos = Tuple[int, int]
-
-
-@dataclass
-class State4(NamedTuple):
-    pos: Tuple[Pos, Pos, Pos, Pos]
-    keys: FrozenSet[set]
-    num_steps: int
+def find_all_keys(world):
+    keys = {}
+    for row in range(len(world)):
+        for col in range(len(world[row])):
+            if world[row][col].islower():
+                keys[world[row][col]] = row, col
+    return keys
 
 
 def reachable_keys_for_pos(start, world):
@@ -94,81 +93,41 @@ def reachable_keys_for_pos(start, world):
     return frozenset(result)
 
 
-def find_shortest_path(world, goal_pos, pos):
+def solve_quadrant(world, start_pos, keys_in_quadrant):
     def w(pos):
         return world[pos[0]][pos[1]]
 
-    visited = {(pos, frozenset())}
-    queue = deque([(pos, frozenset(), 0, "")])
+    start_state = State(start_pos, frozenset(), 0)
+    visited = {(start_state.pos, start_state.keys)}
+    queue = deque([start_state])
     while queue:
-        pos, keys_required, steps, keys_along = queue.popleft()
-        if pos == goal_pos:
-            return (steps, keys_required, keys_along)
-        for neighbour in neighbours(pos):
-            nk = keys_required
-            new_keys_along = keys_along
+        state = queue.popleft()
+        if len(state.keys) == len(keys_in_quadrant):
+            return state.num_steps
+
+        for neighbour in neighbours(state.pos):
+            keys = state.keys
             if w(neighbour) == "#":
                 continue
-            if w(neighbour).isupper():
-                nk = nk | {w(neighbour).lower()}
-            if w(neighbour).islower():
-                new_keys_along = keys_along + w(neighbour)
-            k = (neighbour, nk)
+            elif w(neighbour) in KEYS:
+                keys = keys | {w(neighbour)}
+            elif w(neighbour) in DOORS:
+                required_key = w(neighbour).lower()
+                if required_key in keys_in_quadrant and required_key not in state.keys:
+                    continue
+            k = (neighbour, keys)
             if k not in visited:
                 visited.add(k)
-                queue.append((neighbour, nk, steps + 1, new_keys_along))
-    raise ValueError()
-
-
-def find_all_keys(world):
-    keys = {}
-    for row in range(len(world)):
-        for col in range(len(world[row])):
-            if world[row][col].islower():
-                keys[world[row][col]] = row, col
-    return keys
+                queue.append(State(neighbour, keys, state.num_steps + 1))
 
 
 def b(world):
     starts = find_start_pos(world)
-    all_keys = find_all_keys(world)
     keys_per_start = [reachable_keys_for_pos(start, world) for start in starts]
-    shortest_paths = {}
-    for start, keys in zip(starts, keys_per_start):
-        pois = [start] + [all_keys[k] for k in keys]
-        for i in range(len(pois)):
-            for j in range(i + 1, len(pois)):
-                pos1 = pois[i]
-                pos2 = pois[j]
-                steps, keys_required, keys_along = find_shortest_path(world, pos2, pos1)
-                shortest_paths[pos1, pos2] = steps, keys_required
-                shortest_paths[pos2, pos1] = shortest_paths[pos1, pos2]
-
-    queue = [(0, starts, "")]
-    visited = {""}
-    i = 0
-    while queue:
-        num_steps, positions, collected_keys = heapq.heappop(queue)
-        if num_steps > i:
-            i = num_steps
-            print(i)
-        if len(collected_keys) == len(all_keys):
-            return num_steps
-
-        for robot in range(len(starts)):
-            for key in keys_per_start[robot]:
-                if key in collected_keys:
-                    continue
-
-                key_pos = all_keys[key]
-                steps, keys_required = shortest_paths[positions[robot], key_pos]
-                if all(k in collected_keys for k in keys_required):
-                    new_pos = positions[:robot] + (key_pos,) + positions[robot + 1 :]
-                    new_keys = collected_keys + key
-                    k = new_keys
-                    if k not in visited:
-                        visited.add(k)
-                        heapq.heappush(queue, (num_steps + steps, new_pos, new_keys))
+    return sum(
+        solve_quadrant(world, start, keys)
+        for start, keys in zip(starts, keys_per_start)
+    )
 
 
 def main():
